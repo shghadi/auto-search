fe610c46a4f42971c9b52edfaea2e8d2b340c858
sh_gh <shokoghasemigh@gmail.com>
2025-09-01 23:02:25 +0330

    'style-components'

--- src/components/auto-search.tsx
+++ src/components/auto-search.tsx
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 
 function AutoSearch() {
   const [query, setQuery] = useState("");
+
   return (
-    <>
-      <div>AutoSearch</div>
+    <div className="container max-w-md mx-auto p-6">
+      <h2 className="text-2xl font-semibold mb-4 text-neutral-dark">
+        AutoSearch
+      </h2>
+
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="search...."
+        placeholder="search..."
+        className="w-full rounded-2xl border border-neutral 
+                     px-4 py-2 pl-10 shadow-sm
+                     placeholder-neutral
+                     focus:border-primary focus:ring-2 focus:ring-primary-light 
+                     transition-all duration-200"
       />
-    </>
+    </div>
   );
 }
 
